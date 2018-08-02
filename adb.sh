bc642a57320ff5791b086c64e8f07305f53fb2dc
Ethan Johnson <39628861+InSaNiTy351@users.noreply.github.com>
2018-08-01 20:44:55 -0700

    Delete adb.sh

--- adb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-unzip \platform-tools-latest-linux.zip
-sudo cp platform-tools/adb /usr/bin/adb
-sudo cp platform-tools/fastboot /usr/bin/fastboot
